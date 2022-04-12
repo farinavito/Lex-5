484018b8d23b0ae499b6d9e381b5f9dd46269274
Vito <farinavito@gmail.com>
2022-04-12 21:16:54 +0200

    added all imports

---
+++ tests/test_contract.py
@@ -0,0 +1,9 @@
+from itertools import chain
+import pytest
+import brownie
+from brownie import *
+from brownie import accounts
+from brownie.network import rpc
+from brownie.network.state import Chain
+
+
