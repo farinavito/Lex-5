84ddc07ee38e279938ac3107ba4e51d3196665e9
Vito <farinavito@gmail.com>
2022-04-12 21:26:47 +0200

    added fixtures

--- tests/test_contract.py
+++ tests/test_contract.py
@@ -6,4 +6,14 @@ from brownie import accounts
 from brownie.network import rpc
 from brownie.network.state import Chain
 
+depositAmount = 10**6
+depositLockTime = 86400 #1day
+depositSignee = 9
 
+@pytest.fixture(autouse=True)
+def deploy(Funds):
+    return Funds
+
+@pytest.fixture(autouse=True)
+def deploy_newsafe(deploy):
+    return deploy.deposit(depositAmount, depositLockTime, {'from': accounts[depositSignee]})
