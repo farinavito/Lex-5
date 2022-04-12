053d6f0abfba960da5230dfd93f62675f24a9e76
Vito <farinavito@gmail.com>
2022-04-13 00:03:29 +0200

    add new variables for safe 2

--- tests/test_contract.py
+++ tests/test_contract.py
@@ -6,10 +6,17 @@ from brownie import accounts
 from brownie.network import rpc
 from brownie.network.state import Chain
 
+#new safe -> 1
 depositAmount = 10**6
 depositLockTime = 86400 #1day
 depositSignee = 9
 agreements_number = 1
+
+#new safe -> 2
+depositAmount2 = 10**7
+depositLockTime2 = 86400 #1day
+depositSignee2 = 8
+agreements_number2 = 2
 
 @pytest.fixture(autouse=True)
 def deploy(LockMyFunds):
@@ -21,9 +28,14 @@ deploymentTime = chain.time()
 def deploy_newsafe(deploy):
     return deploy.deposit(depositAmount, depositLockTime, {'from': accounts[depositSignee]})
 
+chain = Chain()
+deploymentTime = chain.time()
+@pytest.fixture(autouse=True)
+def deploy_newsafe2(deploy):
+    return deploy.deposit(depositAmount2, depositLockTime2, {'from': accounts[depositSignee2]})
 
 
-'''TESTING DEPOSIT FUNCTION'''
+'''TESTING DEPOSIT FUNCTION FOR SAFE 1'''
 
 
 
@@ -42,4 +54,10 @@ def test_exactSafe_balances(deploy):
 def test_exactSafe_lockedUpTime(deploy):
     '''check if the first lockedUpTime of the safe is depositAmount'''
     assert deploy.exactSafe(agreements_number)[3] == depositLockTime + deploymentTime + 9
+
+
+
+'''TESTING DEPOSIT FUNCTION FOR SAFE 2'''
+
+
 
